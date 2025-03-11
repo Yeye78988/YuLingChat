063b1901c2d53cd9b07aac64310c5fbb6bd661d8
Kiwi2333 <1329634286@qq.com>
2025-03-12 01:48:46 +0800

    Feature: 图片预览器添加下载图片功能

--- components/card/ElImage.vue
+++ components/card/ElImage.vue
@@ -7,9 +7,11 @@ const props = defineProps<{
   errorRootClass?: string
   errorClass?: string
   ctxName?: string
+  previewSrcList?: string[]
 }>();
 const setting = useSettingStore();
 const src = computed(() => props.defaultSrc ? BaseUrlImg + props.defaultSrc : props.src);
+const preLen = computed(() => (props.previewSrcList?.length || 0) > 1);
 </script>
 
 <template>
@@ -21,6 +23,7 @@ const src = computed(() => props.defaultSrc ? BaseUrlImg + props.defaultSrc : pr
     :close-on-press-escape="!setting.settingPage.isEscMin"
     :draggable="false"
     v-bind="$attrs"
+    :preview-src-list="previewSrcList"
     :ctx-name="ctxName"
   >
     <!-- 占位 -->
@@ -32,6 +35,58 @@ const src = computed(() => props.defaultSrc ? BaseUrlImg + props.defaultSrc : pr
       <div :ctx-name="ctxName" class="h-full w-full flex-row-c-c" :class="errorRootClass">
         <i :ctx-name="ctxName" class="icon i-solar-gallery-remove-bold-duotone op-60" :class="errorClass" />
       </div>
+    </template>
+    <!-- 工具栏 -->
+    <template #toolbar="{ actions, prev, next, reset, activeIndex }">
+      <el-icon-d-arrow-left
+        v-if="preLen"
+        class="btn-primary dark:btn-info"
+        title="上一张"
+        style="width: 1.4rem; height: 1.4rem !important;"
+        @click="prev"
+      />
+      <!-- zoom -->
+      <el-icon-zoom-out
+        class="btn-primary dark:btn-info"
+        title="缩小"
+        style="width: 1.4rem; height: 1.4rem !important;"
+        @click="actions('zoomOut')"
+      />
+      <el-icon-zoom-out
+        class="btn-primary dark:btn-info"
+        title="放大"
+        style="width: 1.4rem; height: 1.4rem !important;"
+        @click="actions('zoomIn')"
+      />
+      <!-- rotate -->
+      <el-icon-refresh-right
+        class="btn-primary dark:btn-info"
+        title="向前旋转"
+        style="width: 1.4rem; height: 1.4rem !important;"
+        @click="actions('clockwise')"
+      />
+      <!-- reset -->
+      <span class="i-tabler:maximize btn-primary dark:btn-info" style="width: 1.4rem; height: 1.4rem !important;" @click="reset" />
+      <el-icon-refresh-left
+        class="btn-primary dark:btn-info"
+        title="重置"
+        style="width: 1.4rem; height: 1.4rem !important;"
+        @click="actions('anticlockwise')"
+      />
+      <el-icon-download
+        v-if="previewSrcList?.[activeIndex]"
+        class="btn-primary dark:btn-info"
+        title="保存"
+        style="width: 1.4rem; height: 1.4rem;"
+        @click="saveImageLocal(previewSrcList?.[activeIndex] as string)"
+      />
+      <el-icon-d-arrow-right
+        v-if="preLen"
+        class="btn-primary dark:btn-info"
+        title="下一张"
+        style="width: 1.4rem; height: 1.4rem !important;"
+        @click="next"
+      />
     </template>
   </el-image>
   <template v-else>
