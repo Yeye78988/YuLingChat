fb0d15eb11c03ee321799f0f8c5b8baeecf5cdf8
Kiwi2333 <1329634286@qq.com>
2025-06-21 00:59:19 +0800

    style(chat): 优化聊天组件样式
    - 调整 ContactList 组件中的样式顺序和命名
    - 优化 MsgTemplate 组件中的翻译功能样式
    - 统一使用 Tailwind CSS 类名，提高代码一致性

--- components/Chat/ContactList.vue
+++ components/Chat/ContactList.vue
@@ -368,7 +368,7 @@ onMounted(() => {
       </MenuPopper>
     </div>
     <div class="relative w-full flex-row-c-c">
-      <div v-if="isSyncing" data-fade style="--anima: latter-slice-bottom;" class="absolute top-4 z-2 flex-row-c-c rounded px-2 py-1 text-theme-primary shadow-lg bg-color-br text-mini">
+      <div v-if="isSyncing" data-fade style="--anima: latter-slice-bottom;" class="absolute top-4 z-2 flex-row-c-c rounded bg-color-br px-2 py-1 text-mini text-theme-primary shadow-lg">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 animate-spin select-none" viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd"><path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" /><path fill="currentColor" d="M12 4.5a7.5 7.5 0 1 0 0 15a7.5 7.5 0 0 0 0-15M1.5 12C1.5 6.201 6.201 1.5 12 1.5S22.5 6.201 22.5 12S17.799 22.5 12 22.5S1.5 17.799 1.5 12" opacity=".1" /><path fill="currentColor" d="M12 4.5a7.46 7.46 0 0 0-5.187 2.083a1.5 1.5 0 0 1-2.075-2.166A10.46 10.46 0 0 1 12 1.5a1.5 1.5 0 0 1 0 3" /></g></svg>
         &nbsp;同步中...
       </div>
@@ -397,7 +397,7 @@ onMounted(() => {
       <!-- 添加骨架屏 -->
       <template #pre>
         <div v-if="isReload" key="skeleton" class="main-bg-color absolute left-0 top-0 z-2 h-full w-full flex-1 overflow-y-hidden">
-          <ChatContactSkeleton v-for="i in 10" :key="i" class="contact" />
+          <ChatContactSkeleton v-for="i in 10" :key="i" class="contact-sky" />
         </div>
       </template>
       <template #default="{ item: room }">
@@ -421,7 +421,7 @@ onMounted(() => {
               :error-class="contactTypeIconClassMap[(room as ChatContactVO).type]"
               :default-src="room.avatar"
               fit="cover"
-              class="h-full w-full card-rounded-df object-cover shadow-sm card-bg-color-2"
+              class="h-full w-full card-rounded-df card-bg-color-2 object-cover shadow-sm"
             />
           </el-badge>
           <div class="flex flex-1 flex-col justify-between truncate">
@@ -431,7 +431,7 @@ onMounted(() => {
               </p>
               <!-- AI机器人 -->
               <i v-if="RoomTypeTagType[room.type]" i-ri:robot-2-line class="ai-icon" />
-              <span class="text ml-a w-fit flex-shrink-0 text-right text-0.7em leading-2em text-color">
+              <span class="text ml-a w-fit flex-shrink-0 text-right text-0.7em text-color leading-2em">
                 {{ formatContactDate(room.activeTime) }}
               </span>
             </div>
@@ -466,6 +466,11 @@ onMounted(() => {
 .main-bg-color {
   --at-apply: "sm:card-bg-color-2 bg-color-3";
 }
+
+.contact-sky {
+  --at-apply: "h-18 card-bg-color dark:bg-transparent flex items-center gap-3 p-4 sm:(h-18 border-transparent p-3 w-full text-color card-rounded-df mb-2 card-bg-color)  w-full text-sm  cursor-pointer  !hover:bg-[#f8f8f8] !dark:hover:bg-[#151515]";
+}
+
 .contact-list {
   --at-apply: "sm:p-2 p-0";
 
